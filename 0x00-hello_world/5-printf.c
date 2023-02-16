13f089927d70617d57e89290c23fcfb73b08dcf3
Linusmwaniki <chombalinus02@gmail.com>
2023-02-16 07:44:27 -0800

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-include <stdio.h>
-
-/**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
+#include <stdio.h>
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("Roses are red,\n"
+           "Violets are blue,\n"
+           "Sugar is sweet,\n"
+           "And so are you.\n");
+
+    return 0;
 }
