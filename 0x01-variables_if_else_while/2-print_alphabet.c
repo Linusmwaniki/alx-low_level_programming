899b1ebffa6fbeea20fb15d8a66a7128ca896053
Linusmwaniki <chombalinus02@gmail.com>
2023-02-17 05:22:01 -0800

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * Main -entry point
+ * print all aphabet letters
+ * Return 0 if successful
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		++ch
+	}
+	putchar('\n);
+	
+	return 0;
+}
