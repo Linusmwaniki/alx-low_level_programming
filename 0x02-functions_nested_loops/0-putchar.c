720f739afd421cc8e4eca47fe4506310caaf0937
Linusmwaniki <chombalinus02@gmail.com>
2023-02-20 23:59:40 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,19 @@
  *
  * Description: prints _putchar followed by a new line
  *
- * Return: (0) if success
+ * Return: (0) if 
  */
-
 int main(void)
 {
-	char ch = '_putchar';
-	putchar(ch\n);
+	char c[8] = "_putchar";
+
+	int i = 0;
+
+	while(i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
