a9ed2bd412cf823b6db9cc6f6fc73fd4cb3b2023
Linusmwaniki <chombalinus02@gmail.com>
2023-02-20 23:49:58 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: (0) if success
+ */
+
+int main(void)
+{
+	char ch = '_putchar';
+	putchar(ch\n);
+	return (0);
+}
